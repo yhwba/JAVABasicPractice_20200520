e1a850ee3be7941c739ea22d007d6d51d15afd14
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-21 10:10:38 +0900

    메인 바인딩

--- app/src/main/java/kr/co/yhw/javabasicpractice_20200520/MainActivity.java
+++ app/src/main/java/kr/co/yhw/javabasicpractice_20200520/MainActivity.java
@@ -1,16 +1,19 @@
 package kr.co.yhw.javabasicpractice_20200520;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import kr.co.yhw.javabasicpractice_20200520.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
